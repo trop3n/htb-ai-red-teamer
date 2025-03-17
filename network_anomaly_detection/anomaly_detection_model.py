a0a98a2e9e51b2284bb2c88f13bd16b024d28a51
trop3n <jasondkimm@proton.me>
2025-03-16 21:25:30 -0500

    added network anomaly detection python file

---
+++ network_anomaly_detection/anomaly_detection_model.py
@@ -0,0 +1,10 @@
+import requests, zipfile, io
+
+# URL for the NSL-KDD dataset
+url = "https://academy.hackthebox.com/storage/modules/292/KDD_dataset.zip"
+
+# Download the zip file and extract its contents
+response = requests.get(url)
+z = zipfile.ZipFile(io.BytesIO(response.content))
+z.extracall('.') # extracts to the current directory
+
