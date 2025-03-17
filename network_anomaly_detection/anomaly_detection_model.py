32da6088a183304064f40280330fa418b8e86927
trop3n <jasondkimm@proton.me>
2025-03-16 21:29:49 -0500

    imported necessary libraries

--- network_anomaly_detection/anomaly_detection_model.py
+++ network_anomaly_detection/anomaly_detection_model.py
@@ -1,4 +1,11 @@
 import requests, zipfile, io
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix, classification_report
 
 # URL for the NSL-KDD dataset
 url = "https://academy.hackthebox.com/storage/modules/292/KDD_dataset.zip"
