dc4231a65bac9757ab0848a6ef0801f729fe894d
trop3n <jasondkimm@proton.me>
2025-03-20 21:55:33 -0500

    began working on assessment submission

---
+++ applications_of_AI_infosec/skills_assessment/assessment.py
@@ -0,0 +1,23 @@
+import requests
+import zipfile
+import io
+import pandas as pd
+import re
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
+import joblib
+import sys
+import json
+
+def download():
+    url = "https://academy.hackthebox.com/storage/modules/292/skills_assessment_data.zip"
+    response = requests.get(url)
+    if response.status_code == 200:
+        print("Download successful")
+        with zipfile.ZipFile(io.BytesIO(response.content)) as z:
+            z.extractall("skills_assessment_data")
+            print("Extraction Successful")
+    else:
+        print("Failed to download the dataset")
