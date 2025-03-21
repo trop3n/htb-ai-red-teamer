60370513c2fe936e413707b951bbd4c1b92385b6
trop3n <jasondkimm@proton.me>
2025-03-20 22:07:11 -0500

    functions added to assessment

--- applications_of_AI_infosec/skills_assessment/assessment.py
+++ applications_of_AI_infosec/skills_assessment/assessment.py
@@ -21,3 +21,29 @@ def download():
             print("Extraction Successful")
     else:
         print("Failed to download the dataset")
+
+def dataset():
+    df = pd.read_json("skills_assessment_data/train.json", orient="records")
+    df.info()
+    # Drop duplicates
+    df = drop_duplicates()
+    return df
+
+def clean_text(text):
+    text = re.sub(r"<.*?>", " ", text)
+    text = re.sub(r"[^\w\s]", " ", text)
+    text = re.sub(r"\s+", " ",).strip()
+    return text
+
+def preprocessing(df):
+    df["text"] = df["text"].apply(lambda x: x.lower())
+    df["text"] = df["text"] = df.apply(clean_text)
+    return df
+
+def train_model(df):
+    X_train, X_test, y_train, y_test = train_test_split(
+        df["text"], df["label"], test_size=0.3, random_state=42
+    )
+    pipeline = Pipeline([
+        
+    ])
